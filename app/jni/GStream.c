ce200546a0330b8d52d632a6cea3b64af2125cb8
JKlasen531 <j.klasen@hotmail.de>
2021-01-11 13:50:47 +0100

    GStreamer implementation, wip

--- app/jni/GStream.c
+++ app/jni/GStream.c
@@ -246,7 +246,7 @@ gst_native_init (JNIEnv * env, jobject thiz)
     SET_CUSTOM_DATA (env, thiz, custom_data_field_id, data);
     GST_DEBUG_CATEGORY_INIT (debug_category, "lawnmower", 0,
                              "Android lawnmower");
-    gst_debug_set_threshold_for_name ("tutorial-3", GST_LEVEL_DEBUG);
+    gst_debug_set_threshold_for_name ("lawnmower", GST_LEVEL_DEBUG);
     GST_DEBUG ("Created CustomData at %p", data);
     data->app = (*env)->NewGlobalRef (env, thiz);
     GST_DEBUG ("Created GlobalRef for app object at %p", data->app);
